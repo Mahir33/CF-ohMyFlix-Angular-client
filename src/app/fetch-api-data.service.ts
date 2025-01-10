d7088f7d9500c07d1511debbbbf9521e970561ce
Mahir33 <adam.marek.wrobel@gmail.com>
2025-01-10 16:51:01 +0100

    adding api endpoints

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,23 +5,90 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://myflix-api-mahir-941afb3e93ba.herokuapp.com/';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserRegistrationService {
-  // Inject the HttpClient module to the constructor params
- // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {
+
+apiUrl = 'https://myflix-api-mahir-941afb3e93ba.herokuapp.com/';
+ 
+ constructor(private http: HttpClient) {
     this.http = http;
   }
- // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
-    catchError(this.handleError)
-    );
-  }
+ // User registration
+ public userRegistration(userDetails: any) {
+  return this.http
+    .post(`${this.apiUrl}/users`, userDetails)
+    .pipe(catchError(this.handleError));
+}
+// User login
+public userLogin(userDetails: any) {
+  return this.http
+    .post(`${this.apiUrl}/login`, userDetails)
+    .pipe(catchError(this.handleError));
+}
+// Get all movies
+public getAllMovies() {
+  return this.http
+    .get(`${this.apiUrl}/movies`)
+    .pipe(catchError(this.handleError));
+}
+// Get one movie
+public getOneMovie(title: string) {
+  return this.http
+    .get(`${this.apiUrl}/movies/${title}`)
+    .pipe(catchError(this.handleError));
+}
+// Get director
+public getDirector(name: string) {
+  return this.http
+    .get(`${this.apiUrl}/directors/${name}`)
+    .pipe(catchError(this.handleError));
+}
+// Get genre
+public getGenre(name: string) {
+  return this.http
+    .get(`${this.apiUrl}/genres/${name}`)
+    .pipe(catchError(this.handleError));
+}
+// Get user
+public getUser(username: string) {
+  return this.http
+    .get(`${this.apiUrl}/users/${username}`)
+    .pipe(catchError(this.handleError));
+}
+// Get favorite movies for a user
+public getFavoriteMovies(username: string) {
+  return this.http
+    .get(`${this.apiUrl}/users/${username}/movies`)
+    .pipe(catchError(this.handleError));
+}
+// Add a movie to favorite movies
+public addFavoriteMovie(username: string, movieId: string) {
+  return this.http
+    .post(`${this.apiUrl}/users/${username}/movies/${movieId}`, {})
+    .pipe(catchError(this.handleError));
+}
+// Edit user
+public editUser(username: string, userDetails: any) {
+  return this.http
+    .put(`${this.apiUrl}/users/${username}`, userDetails)
+    .pipe(catchError(this.handleError));
+}
+// Delete user
+public deleteUser(username: string) {
+  return this.http
+    .delete(`${this.apiUrl}/users/${username}`)
+    .pipe(catchError(this.handleError));
+}
+// Delete a movie from favorite movies
+public deleteFavoriteMovie(username: string, movieId: string) {
+  return this.http
+    .delete(`${this.apiUrl}/users/${username}/movies/${movieId}`)
+    .pipe(catchError(this.handleError));
+}
+// Handle API errors
 
 private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
